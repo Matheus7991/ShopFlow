d77d311b501b49c6c39795f6c2e105f99559ff14
Matheus <bandeira.freitas2013@gmail.com>
2026-08-22 13:53:11 -0300

    Implementando estrutura de repositórios para consulta de end-points.

---
+++ auth-service/src/main/java/com/shopflow/auth/infrastructure/persistence/repository/SpringDataUserRepository.java
@@ -0,0 +1,15 @@
+package com.shopflow.auth.infrastructure.persistence.repository;
+
+import com.shopflow.auth.infrastructure.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SpringDataUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
--- auth-service/src/main/java/com/shopflow/auth/infrastructure/persistence/repository/UserRepositoryImpl.java
+++ auth-service/src/main/java/com/shopflow/auth/infrastructure/persistence/repository/UserRepositoryImpl.java
@@ -1,4 +1,44 @@
 package com.shopflow.auth.infrastructure.persistence.repository;
 
-public class UserRepositoryImpl {
+import com.shopflow.auth.domain.model.User;
+import com.shopflow.auth.domain.repository.UserRepository;
+import com.shopflow.auth.infrastructure.persistence.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final SpringDataUserRepository repository;
+
+    @Override
+    public User save(User user) {
+        var entity = UserMapper.toEntity(user);
+        var savedEntity = repository.save(entity);
+
+        return UserMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return repository.findById(id)
+                .map(UserMapper::toDomain);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return repository.findByEmail(email)
+                .map(UserMapper::toDomain);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+
+
 }
