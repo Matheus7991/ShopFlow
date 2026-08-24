6d3d95d798a58a41047e6e905eba9ab4b702cf59
Matheus <bandeira.freitas2013@gmail.com>
2026-08-23 21:33:41 -0300

    Implementação do primeiro teste de integração

--- auth-service/src/test/java/com/shopflow/auth/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/shopflow/auth/AuthServiceApplicationTests.java
@@ -2,9 +2,31 @@ package com.shopflow.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest
 class AuthServiceApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:16-alpine")
+					.withDatabaseName("shopflow_auth")
+					.withUsername("shopflow")
+					.withPassword("shopflow");
+
+	@DynamicPropertySource
+	static void configurePropeties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
